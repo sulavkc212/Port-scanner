7f786398569c9cd31e6291f87852d5ac4470e5ad
Sulav Katwal <42937315+sulavkc212@users.noreply.github.com>
2023-02-04 17:50:12 +0545

    Add files via upload

---
+++ open port scanner.py
@@ -0,0 +1,10 @@
+import socket
+
+target_host = "www.google.com"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host,target_port))
+
+print("Port Scanner has finished scanning.")
